9ad15c47494a1385af63bb69567c8c746b1a8421
garth <52281882+g-ju@users.noreply.github.com>
2023-11-04 09:04:32 +0000

    Rename package

--- src/main/java/main/ActivityApplication.java
+++ src/main/java/main/ActivityApplication.java
@@ -1,4 +1,4 @@
-package activity;
+package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/main/ActivityApplicationTests.java
+++ src/test/java/main/ActivityApplicationTests.java
@@ -1,4 +1,4 @@
-package activity;
+package main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
